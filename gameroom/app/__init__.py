ca6f03f91a3047d5f3e095fc391d6ecec8a6d872
Michael Cole <mcole042891@gmail.com>
2019-12-13 18:58:57 -0600

    [US0006]: Set up SQLAlchemy

--- gameroom/app/__init__.py
+++ gameroom/app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object('config.Config')
+    db.init_app(app)
 
     with app.app_context():
 
--- gameroom/config.py
+++ gameroom/config.py
@@ -10,3 +10,11 @@ class Config:
     FLASK_DEBUG = environ['FLASK_DEBUG']
     FLASK_HOST = environ['FLASK_HOST']
     FLASK_PORT = environ['FLASK_PORT']
+
+    # mysql config
+    MYSQL_DATABASE = environ['MYSQL_DATABASE']
+    MYSQL_ROOT_PASSWORD = environ['MYSQL_ROOT_PASSWORD']
+
+    # sqlalchemy config
+    SQLALCHEMY_DATABASE_URI = environ['SQLALCHEMY_DATABASE_URI']
+    SQLALCHEMY_TRACK_MODIFICATIONS = environ['SQLALCHEMY_TRACK_MODIFICATIONS']
