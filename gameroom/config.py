79ed16a3102d81c92a023ba04ae152818f856e90
Michael Cole <mcole042891@gmail.com>
2019-12-18 22:18:42 -0600

    [US0013]: Changed secret key from 32 to 16

--- gameroom/config.py
+++ gameroom/config.py
@@ -4,7 +4,7 @@ from os import urandom, environ
 class Config:
 
     # app config
-    SECRET_KEY = urandom(32)
+    SECRET_KEY = urandom(16)
     FLASK_APP = environ["FLASK_APP"]
     FLASK_ENV = environ["FLASK_ENV"]
     FLASK_DEBUG = environ['FLASK_DEBUG']
@@ -14,7 +14,7 @@ class Config:
     DB_WAIT_INITIAL = environ['DB_WAIT_INITIAL']
     DB_WAIT_MULTIPLIER = environ['DB_WAIT_MULTIPLIER']
     DB_WAIT_MAX = environ['DB_WAIT_MAX']
-   
+
     # mysql config
     MYSQL_DATABASE = environ['MYSQL_DATABASE']
     MYSQL_ROOT_PASSWORD = environ['MYSQL_ROOT_PASSWORD']
